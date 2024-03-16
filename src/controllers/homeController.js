bed17225d6f78ead639f0cbe7348885c8fcd0e76
truongthan <truongthanxlscpy@gmail.com>
2024-03-16 14:54:16 +0700

    insert to table when receive data from brokerMQTT

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,13 @@
 import Service from '../services/service.js'; 
+import MQTTClient from '../services/mqttClientService.js';
 
+
+const brokerUrl = 'mqtt://io.adafruit.com';
+const username = 'nguyentruongthan';
+const password = 'aio_vuMN25byK98NvxriL7############7hAfLe5KyK';
+const topics = ['tempurature', 'humidity', 'light', 'nutnhan1', 'nutnhan2'];
+
+const mqttClient = new MQTTClient(brokerUrl, username, password, topics);
 
 let getHomePage = async (req, res) => {
   try {
--- src/services/mqttClientService.js
+++ src/services/mqttClientService.js
@@ -1,4 +1,5 @@
 import mqtt from 'mqtt';
+import db from '../models/index.js';
 
 class MQTTClient {
   constructor(brokerUrl, username, password, topics) {
@@ -19,9 +20,33 @@ class MQTTClient {
       });
     });
 
-    this.client.on('message', (topic, message) => {
+    this.client.on('message', async (topic, message) => {
       console.log(`Received message from ${topic}: ${message.toString()}`);
       // Xử lý dữ liệu nhận được tại đây
+      topic = topic.split('/')[2];
+      switch (topic) {
+        case 'tempurature':
+          // Lưu dữ liệu vào database
+          await db.Temp.create({ value: message.toString() });
+          break;
+        case 'humidity':
+          await db.Humi.create({ value: message.toString() });
+          // Lưu dữ liệu vào database
+          break;
+        case 'light':
+          await db.Light.create({ value: message.toString() });
+          // Lưu dữ liệu vào database
+          break;
+        case 'nutnhan1':
+          // Lưu dữ liệu vào database
+          break;
+        case 'nutnhan2':
+          // Lưu dữ liệu vào database
+          break;
+        default:
+          break;
+      }
+     
     });
 
     this.client.on('close', () => {
